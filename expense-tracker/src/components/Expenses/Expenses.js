14d886972d4fb82d2f37d843a86b9281207dbce3
piyush-xo <kpiyush381@gmail.com>
2022-12-12 23:08:50 +0530

    added expense filter

--- expense-tracker/src/components/Expenses/Expenses.js
+++ expense-tracker/src/components/Expenses/Expenses.js
@@ -1,10 +1,19 @@
+import React, {useState} from 'react';
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState('2022');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  }
+
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onFilterChange={filterChangeHandler}></ExpensesFilter>
       <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
