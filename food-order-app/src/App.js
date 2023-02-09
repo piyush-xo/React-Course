a237a9deee864990dd2d7ddbf6113b4ad1290008
piyush-xo <kpiyush381@gmail.com>
2023-02-09 22:34:26 +0530

    Added Modal

--- food-order-app/src/App.js
+++ food-order-app/src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
+import { useState } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <React.Fragment>
-      <Header></Header>
+      {cartIsShown && <Cart onCloseCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
-        <Meals></Meals>
+        <Meals />
       </main>
     </React.Fragment>
   );
--- food-order-app/src/components/Cart/Cart.js
+++ food-order-app/src/components/Cart/Cart.js
@@ -1,27 +1,30 @@
 import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
 
 const Cart = (props) => {
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map(
-        (cartItem) => {
-          <li>{cartItem.name}</li>;
-        }
+        (cartItem) => (
+          <li>{cartItem.name}</li>
+        )
       )}
     </ul>
   );
   return (
-    <div>
+    <Modal onCloseCart={props.onCloseCart}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
-      <div>
-        <button className={classes["button--alt"]}>Close</button>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]} onClick={props.onCloseCart}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
