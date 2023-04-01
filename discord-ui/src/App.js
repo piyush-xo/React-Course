4fea52f2d5fdcb01a010706edcf14a63475a4f2b
piyush-xo <kpiyush381@gmail.com>
2023-04-01 19:37:54 +0530

    added sidebar component

--- discord-ui/src/App.js
+++ discord-ui/src/App.js
@@ -1,8 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
+import SideBar from "./SideBar";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex">
+      <SideBar />
+    </div>
+  );
 }
 
 export default App;
--- discord-ui/tailwind.config.js
+++ discord-ui/tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   mode: 'jit',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#202225',
+        secondary: '#5865f2',
+        gray: colors.trueGray,
+        gray: {
+          900: '#202225',
+        }
+      }
+    },
   },
   plugins: [],
 }
