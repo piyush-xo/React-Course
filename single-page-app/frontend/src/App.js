a4d04b875aff344a6cc14548ed9fc4f5d108056d
piyush-xo <kpiyush381@gmail.com>
2023-04-02 10:45:17 +0530

    added events root layout

--- single-page-app/frontend/src/App.js
+++ single-page-app/frontend/src/App.js
@@ -27,6 +27,7 @@ import EventDetailPage from "../src/pages/EventDetailPage";
 import NewEventPage from "../src/pages/NewEventPage";
 import EditEventPage from "../src/pages/EditEventPage";
 import RootLayout from "./pages/Roots";
+import EventsRootLayout from "./pages/EventsRoot";
 
 const router = createBrowserRouter([
   {
@@ -34,10 +35,17 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: "events", element: <EventsPage /> },
-      { path: "events/:eventId", element: <EventDetailPage /> },
-      { path: "events/new", element: <NewEventPage /> },
-      { path: "events/:eventId/edit", element: <EditEventPage /> },
+      {
+        path: "events",
+        element: <EventsRootLayout />,
+        children: [
+          { index: true, element: <EventsPage /> },
+          { path: ":eventId", element: <EventDetailPage /> },
+          { path: "new", element: <NewEventPage /> },
+          { path: ":eventId/edit", element: <EditEventPage /> },
+        ],
+      },
+      ,
     ],
   },
 ]);
