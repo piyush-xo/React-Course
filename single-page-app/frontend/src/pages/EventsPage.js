e38fb460ef304c519a5bb9b1eb2e8a35cf531d31
piyush-xo <kpiyush381@gmail.com>
2023-04-02 16:48:33 +0530

    changed response for loader

--- single-page-app/frontend/src/pages/EventsPage.js
+++ single-page-app/frontend/src/pages/EventsPage.js
@@ -2,21 +2,21 @@ import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const events = useLoaderData();
-  return <EventsList events={events} />;
+  const loaderData = useLoaderData();
+  return <EventsList events={loaderData.events} />;
 }
 
 export default EventsPage;
 
 export async function loader() {
-  // loader function gets executed even before the page is opened by react, 
+  // loader function gets executed even before the page is opened by react,
   // in other words, React will load the page only after the loader function completes it's execution
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    //...
+    // return response object with error and catch it in the ui
+    throw { message: "Could not fetch events." };
   } else {
-    const resData = await response.json();
-    return resData.events;
+    return response;
   }
 }
