bdcb354c1f8bde322ddefb32ab6b159eed6a91b8
piyush-xo <kpiyush381@gmail.com>
2023-04-03 22:46:33 +0530

    added submitting feedback in eventform

--- single-page-app/frontend/src/components/EventForm.js
+++ single-page-app/frontend/src/components/EventForm.js
@@ -1,38 +1,68 @@
-import { useNavigate, Form } from 'react-router-dom';
+import { useNavigate, Form, useNavigation } from "react-router-dom";
 
-import classes from './EventForm.module.css';
+import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
   const navigate = useNavigate();
+  const navigaiton = useNavigation();
+
+  const isSubmitting = navigaiton.state === "submitting";
+
   function cancelHandler() {
-    navigate('..');
+    navigate("..");
   }
 
   return (
     //special Form tag from router, which sends form data to your action
     // unlike the default browser action of sending form data to backend
-    <Form method='POST' className={classes.form}>
+    <Form method="POST" className={classes.form}>
       <p>
         <label htmlFor="title">Title</label>
-        <input id="title" type="text" name="title" defaultValue={event && event.title} required />
+        <input
+          id="title"
+          type="text"
+          name="title"
+          defaultValue={event && event.title}
+          required
+        />
       </p>
       <p>
         <label htmlFor="image">Image</label>
-        <input id="image" type="url" name="image" defaultValue={event && event.image} required />
+        <input
+          id="image"
+          type="url"
+          name="image"
+          defaultValue={event && event.image}
+          required
+        />
       </p>
       <p>
         <label htmlFor="date">Date</label>
-        <input id="date" type="date" name="date" defaultValue={event && event.date} required />
+        <input
+          id="date"
+          type="date"
+          name="date"
+          defaultValue={event && event.date}
+          required
+        />
       </p>
       <p>
         <label htmlFor="description">Description</label>
-        <textarea id="description" name="description" rows="5" defaultValue={event && event.description} required />
+        <textarea
+          id="description"
+          name="description"
+          rows="5"
+          defaultValue={event && event.description}
+          required
+        />
       </p>
       <div className={classes.actions}>
-        <button type="button" onClick={cancelHandler}>
+        <button type="button" disabled={isSubmitting} onClick={cancelHandler}>
           Cancel
         </button>
-        <button>Save</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? "Submitting..." : "Save"}
+        </button>
       </div>
     </Form>
   );
