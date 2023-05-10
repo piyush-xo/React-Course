130c5cc6127fe810ebcf1262956c4384ae8e9ae2
piyush-xo <kpiyush381@gmail.com>
2023-05-10 22:42:05 +0530

    redux toolkit

--- react-redux/src/components/Counter.js
+++ react-redux/src/components/Counter.js
@@ -1,25 +1,41 @@
 import classes from "./Counter.module.css";
 import { useSelector, useDispatch } from "react-redux";
+import { counterActions } from "../store/index"; 
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
   const showCounter = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
 
+  // const incrementHandler = () => {
+  //   dispatch({ type: "increment" });
+  // };
+
+  // const increaseHandler = () => {
+  //   dispatch({ type: "increase", amount: 50 });
+  // };
+
+  // const decrementHandler = () => {
+  //   dispatch({ type: "decrement" });
+  // };
+
+  // const toggleCounterHandler = () => {
+  //   dispatch({ type: "toggle" });
+  // };
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment()); // counterActions.increment() returns an action object which is dispatched
   };
 
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 50 });
+    dispatch(counterActions.increase(10)); // returns an object {type: "UNIQUE_IDENTIFIER", payload: 10}
   };
 
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
