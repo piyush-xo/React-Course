99711286ad8cd90e48eca74d648658d3d88b49ad
piyush-xo <kpiyush381@gmail.com>
2023-05-21 17:56:38 +0530

    done

--- pagination-app/src/App.js
+++ pagination-app/src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [todosPerPage, setTodosPerPage] = useState(10);
+
+  const noOfPages = Math.ceil(todos.length / todosPerPage); //10
+  const pages = [...Array(noOfPages + 1).keys()].slice(1);
+  const indexOfLastTodo = todosPerPage * currentPage;
+  const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
+  console.log(todosPerPage, indexOfFirstTodo, indexOfLastTodo);
+  const visibleTodos = todos.slice(indexOfFirstTodo, indexOfLastTodo);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => setTodos(res.data));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="App-header">Pagination</div>
+      <select
+        style={{ margin: "1rem" }}
+        onChange={(e) => {
+          setCurrentPage(1);
+          setTodosPerPage(e.target.value);
+        }}
+      >
+        <option value="10">10</option>
+        <option value="20">20</option>
+        <option value="50">50</option>
+      </select>
+      <div>
+        {visibleTodos.map((todo) => (
+          <p
+            key={todo.id}
+            className={todo.completed === true ? "completed" : "notCompleted"}
+          >
+            {todo.id}. {todo.title}
+          </p>
+        ))}
+      </div>
+      <span>
+        <p
+          style={{ cursor: "pointer" }}
+          onClick={() => {
+            if (currentPage !== 1) {
+              setCurrentPage(currentPage - 1);
+            }
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Prev
+        </p>
+        {pages.map((page) => (
+          <>
+            <span
+              key={page}
+              className={page === currentPage ? "active" : ""}
+              style={{ cursor: "pointer" }}
+              onClick={() => setCurrentPage(page)}
+            >{` ${page} `}</span>
+            <span>|</span>
+          </>
+        ))}
+        <p
+          style={{ cursor: "pointer" }}
+          onClick={() => {
+            if (currentPage !== noOfPages) {
+              setCurrentPage(currentPage + 1);
+            }
+          }}
+        >
+          Next
+        </p>
+      </span>
     </div>
   );
 }
