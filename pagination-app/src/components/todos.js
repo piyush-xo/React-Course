7c547c44fe7cd8459915970058328f31bb4e7a30
piyush-xo <kpiyush381@gmail.com>
2023-06-13 14:38:49 +0530

    refactored props

--- pagination-app/src/components/todos.js
+++ pagination-app/src/components/todos.js
@@ -46,7 +46,12 @@ const Todos = () => {
             </p>
           ))}
         </div>
-        <PageNavigation pages={pages} currentPage={currentPage} noOfPages={noOfPages} setCurrentPage={setCurrentPage}/>
+        <PageNavigation {...{pages, currentPage, noOfPages, setCurrentPage}}
+          // pages={pages}
+          // currentPage={currentPage}
+          // noOfPages={noOfPages}
+          // setCurrentPage={setCurrentPage}
+        />
       </div>
     </div>
   );
