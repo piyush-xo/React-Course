8120256b7a1aeedf602c723f26b0f2983f3ca061
piyush-xo <kpiyush381@gmail.com>
2023-06-17 23:18:26 +0530

    fixes

--- pagination-app/src/store/store.js
+++ pagination-app/src/store/store.js
@@ -1,4 +1,4 @@
-import { configureStore, createSlice, current } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 let todos = [];
